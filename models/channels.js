e766ab5867ec418ae0c4e0ef4fdbefaef290d0cc
Nicolas Brent Ellerbe <nbrente@gmail.com>
2018-05-03 17:21:03 -0400

    FIXED: .gitignore ADDED: channels.js model

---
+++ models/channels.js
@@ -0,0 +1,17 @@
+module.exports = function (sequelize, DataTypes) {
+  var Channels = sequelize.define("Channels", {
+    // The email cannot be null, and must be a proper email before creation
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    // The password cannot be null
+    password: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  });
+}
