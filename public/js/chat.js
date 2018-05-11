4e502a762b5d6eec1e8eb1a387be9733c0d0f361
Nicolas Brent Ellerbe <nbrente@gmail.com>
2018-05-11 17:45:12 -0400

    borderline functional chat.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,24 +1,31 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var socket = io.connect('http://localhost:8080');
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+
+//Query DOM
+var message = document.getElementById('message'),
+  btn = document.getElementById('send'),
+  output = document.getElementById('output');
+
+var handle;
+
+$.get("api/username", function (req, res) {
+  // Make sure the data contains the username as expected before using it
+  if (data.hasOwnProperty('username')) {
+    console.log('Username: ' + req.user.username);
+    handle = req.user.username;
+  }
 });
 
-io.on('connection', function(socket){
-
-  socket.on('chat message', function(data){
-    io.emit('new message', {msg: data, id: socket.username});
-   
-    
-    console.log(data)
-    
-
-  });
+//Emit Events
+btn.addEventListener('click', function () {
+  socket.emit('chat', {
+    message: message.value,
+    handle: handle.value
+  })
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+
+//Listen for events
+socket.on('chat', function (data) {
+  output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
 });
