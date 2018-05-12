da1475a776006de7fae937a7a3cbd10d46206b72
brint hansen <hansenb@redeyeusa.com>
2018-05-11 21:18:00 -0400

    updated chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,9 @@
 
-
-var socket = io.connect(process.env.PORT || 8080);
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 5000;
 
 
 //Query DOM
